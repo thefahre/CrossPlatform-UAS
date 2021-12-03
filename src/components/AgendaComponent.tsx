67253152c94496d1c07155a4a54131d3068997f6
RenzKanade <61531897+RenzKanade@users.noreply.github.com>
2021-12-03 14:12:50 +0700

    testing agenda

--- src/components/AgendaComponent.tsx
+++ src/components/AgendaComponent.tsx
@@ -1,19 +1,25 @@
 import { IonCard, IonCardTitle, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonText, IonTitle } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 
 const AgendaComponent:React.FC = () =>{
+
+
     return(
+        <>
         <IonItemSliding>
             <IonItemOptions side="start">
                 <IonItemOption routerLink="/editEvent">Edit</IonItemOption>
             </IonItemOptions>
             <IonItem>
-            <IonCard>
-                <IonCardTitle>Event Title</IonCardTitle>
-                <IonText>Event Type | Location  | Start Time | (Optional) End Time </IonText>
-            </IonCard>
+                <IonCard>
+                    <IonCardTitle>Event Title</IonCardTitle>
+                    <IonText>Event Type | Location  | Start Time | (Optional) End Time </IonText>
+                </IonCard>
             </IonItem>
         </IonItemSliding>
+        </>
+        
+
     );
 };
 
